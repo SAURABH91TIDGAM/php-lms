84f6ff5b2c97020847eda28cd1340cc8759542f8
SAURABH91TIDGAM <saurabhtidgam1991@gmail.com>
2023-04-21 12:24:03 +0530

    validating phone number
    poper validation of phone number.

--- process-form.php
+++ process-form.php
@@ -1,15 +1,18 @@
 <?php
-
+require "config.php";
 
 function validating($phone){
-    if(! preg_match('/^[0-9]{10}+$/', $phone))
+    if( preg_match('/^[0-9]{10}+$/', $phone))
     {
-        die("Invalid Phone Number");
+       return True;
+    }
+    else{
+        return false;
     }
 }
 $name = $_POST['name'];
 $contact_number = $_POST['contact_number'];
-validating($contact_number);
+$valid = validating($contact_number);
 
 
 $address = $_POST['address'];
@@ -18,26 +21,32 @@ $state = $_POST['state'];
 $employment_status = $_POST['employment_type'];
 $Loan_status = $_POST['existing_loan'];
 
-require "config.php";
 
 
+if ($valid){
 
-$sql = "INSERT INTO lead_data(Lead_Name, Contact_number, Address, City, State_name, Employment_type, Loan_status)
+    $sql = "INSERT INTO lead_data(Lead_Name, Contact_number, Address, City, State_name, Employment_type, Loan_status)
         VALUES (?,?,?,?,?,?,?)";
 
 
-$stmt =mysqli_stmt_init($connection);
+    $stmt =mysqli_stmt_init($connection);
 
-if( ! mysqli_stmt_prepare($stmt,$sql)){
-    die(mysqli_error($connection));
-};
+    if( ! mysqli_stmt_prepare($stmt,$sql)){
+        die(mysqli_error($connection));
+    };
 
-mysqli_stmt_bind_param($stmt, "sisssss",$name,$contact_number, $address, $city, $state, $employment_status, $Loan_status);
+    mysqli_stmt_bind_param($stmt, "sisssss",$name,$contact_number, $address, $city, $state, $employment_status, $Loan_status);
 
-mysqli_stmt_execute($stmt);
+    mysqli_stmt_execute($stmt);
 
 //echo "record saved";
-header('location:index.php');
+    header('location:index.php');
+
+}
+else{
+    echo "you provided invalid phone number. ";
+    echo "<a href='add_lead_form.php'>GO BACK</a>";
+}
 
 
 
