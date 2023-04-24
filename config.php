629d66b7c2a2e4af7b2788a6bcbe2b395c242963
SAURABH91TIDGAM <saurabhtidgam1991@gmail.com>
2023-04-24 18:39:41 +0530

    Update config.php
    added query to create database if not present and table

--- config.php
+++ config.php
@@ -8,8 +8,23 @@ $password = "MYSQL_saurabh@5378*";
 
 $connection = mysqli_connect($host, $username, $password, $dbname);
 
-if (mysqli_connect_errno()) {
-    die('connection error' . mysqli_connect_error());
+if (!$connection)
+{
+    $sql_query = "CREATE DATABASE myapp";
+    if (mysqli_query($connection, $sql_query))
+    {
+        $sql_query = "CREATE TABLE Persons (id int,Lead_name text,Contact_number bigint,Address varchar(255),City text,State_name text,Employment_type text,Loan_status text)";
+        if(!mysqli_query($connection,$sql_query)){
+            echo "error creating table".mysqli_error();
+        }
+        else
+        {
+            echo "created table successfully";
+        }
+    }
+    else
+    {
+        echo "Error creating database: " . mysqli_error($connection);
+    }
 }
-
 //echo "connection sucessful";
