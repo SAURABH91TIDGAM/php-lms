c27d010941a83072e41b0eb7b4fff4ba52da6cf5
SAURABH91TIDGAM <saurabhtidgam1991@gmail.com>
2023-04-24 15:45:09 +0530

    Update add_lead_form.php
    no need of update form seprately

--- add_lead_form.php
+++ add_lead_form.php
@@ -6,129 +6,161 @@
 </head>
 <body>
 <h1>Lead Management  System</h1>
-<h2>form to Add lead</h2>
+<h2>form to Add/Update lead</h2>
 
-     <form action="process-form.php"  method="post">
-          <div>
-               <label for="name"> Full Name </label>
-               <input type="text" id="name" name="name">
-          </div>
-          <br>
-          <div>
-               <label for="contact number">Mobile Number</label>
-               <input type="number" id="contact number" name="contact_number">
-          </div>
-          <br>
-          <div>
-               <label for="address"> Address </label>
-               <textarea id="address" name="address"></textarea>
-          </div>
-          <br>
-          <div>
-               <label for="city"> City </label>
-               <select id="city" name="city">
-                    <option value="delhi">Delhi</option>
-                    <option value="mumbai">Mumbai</option>
-                    <option value="chennai">Chennai</option>
-                    <option value="kolkata">Kolkata</option>
-                    <option value="gurugram">Gurugram</option>
-                    <option value="pune">Pune</option>
-                    <option value="bengaluru">Bangaluru</option>
-                    <option value="ahemdabad">Ahemdabad</option>
-                    <option value="nagpur">Nagpur</option>
-               </select>
 
-               <label for="state">State</label>
-               <select id="state" name="state">
-                    <option value="delhi">Delhi</option>
-                    <option value="maharashtra">Maharashtra</option>
-                    <option value="tamil nadu">Tamil Nadu</option>
-                    <option value="west bengal">west bengal</option>
-                    <option value="haryana">Haryana</option>
-                    <option value="karnataka">Karnataka</option>
-                    <option value="Gujarat">Gujarat</option>
-               </select>
-
-          </div>
-          <br>
-          <div>
-              <legend>Employment Type</legend>
-               <label><input type="radio" value="salaried" name="employment_type" />Salaried</label>
-               <label><input type="radio" value="Self employed" name="employment_type" />Self employed</label>
-               <label><input type="radio" value="unemployed" name="employment_type" />Not employed</label>
-
-          </div>
-          <br>
-          <div>
-
-               <legend>Existing Loan</legend>
-               <label><input type="radio" name="existing_loan" value="no">no</label><br>
-               <label><input type="radio" name="existing_loan" value="yes">yes</label><br>
-
-          </div>
-
-          <br>
-          <button type="submit">Submit</button>
-          <button type="reset">Reset</button>
-
-     </form>
+<?php
+require "config.php";
 
 
 
-<div class="container">
-     <table class="table">
-          <thead>
-          <tr>
-               <th scope="col">Id</th>
-               <th scope="col">Lead name</th>
-               <th scope="col">Contact number</th>
-               <th scope="col">Address</th>
-               <th scope="col">City</th>
-               <th scope="col">State</th>
-               <th scope="col">Employment</th>
-               <th scope="col">Existing loan</th>
-          </tr>
-          </thead>
-          <tbody>
-          <?php
-          require "config.php";
-          $query = "select * from lead_data";
-          $result = mysqli_query($connection, $query);
-          while($row=mysqli_fetch_assoc($result))
-          {
-               $id=$row['id'];
-               $name=$row['Lead_name'];
-               $number=$row['Contact_number'];
-               $address=$row['Address'];
-               $city=$row['City'];
-               $state=$row['State_name'];
-               $employment_status=$row['Employment_type'];
-               $existing_loan=$row['Loan_status'];
-               echo"
-            <tr><th scope='row'>$id</th>
-            <td>$name</td>
-            <td>$number</td>
-            <td>$address</td>
-            <td>$city</td>
-            <td>$state</td>
-            <td>$employment_status</td>
-            <td>$existing_loan</td>
-            <td><button><a href='update.php?update_id=$id'>Update</a></button></td>
-            <td><button><a href='delete.php?delete_id=$id'>Delete</a></button></td>
-            </tr>
-            ";
-          }
 
-          ?>
 
-          </tbody>
-     </table>
-</div>
+
+if (!isset($_GET["update_id"]))
+{
+     $name = '';
+     $number='';
+     $address='';
+     $city='';
+     $SN='';
+     $ET='';
+     $loan='';
+
+     $action_performed = 'process-form.php';
+}
+else{
+
+
+     $id=$_GET["update_id"];
+
+
+     $sql_query = "select * from lead_data where id='$id'";
+     $result=mysqli_query($connection, $sql_query);
+     $row=mysqli_fetch_assoc($result);
+
+     $name = $row['Lead_name'];
+     $number=$row['Contact_number'];
+     $address=$row['Address'];
+     $city=$row['City'];
+     $SN=$row['State_name'];
+     $ET=$row['Employment_type'];
+     $loan=$row['Loan_status'];
+
+     $action_performed = "process-form.php?id=$id";
+}
+?>
+
+
+
+<form action="<?php echo $action_performed; ?>"  method="post">
+     <div>
+          <label for="name"> Full Name </label>
+          <input type="text" id="name" name="name" value=<?php echo $name;?>>
+     </div><br>
+     <div>
+          <label for="contact number">Mobile Number</label>
+          <input type="number" id="contact number" name="contact_number" value=<?php echo $number?>>
+     </div><br>
+     <div>
+          <label for="address"> Address </label>
+          <textarea id="address" name="address"><?php echo $address?></textarea>
+     </div><br>
+     <div>
+          <label for="city"> City </label>
+          <select id="city" name="city">
+               <option value="delhi"<?php if($city ==='delhi'){ echo "selected";} ?>>Delhi</option>
+               <option value="mumbai" <?php if($city ==='mumbai'){ echo "selected";} ?>>Mumbai</option>
+               <option value="chennai"<?php if($city ==='chennai'){ echo "selected";} ?>>Chennai</option>
+               <option value="kolkata"<?php if($city ==='kolkata'){ echo "selected";} ?>>Kolkata</option>
+               <option value="gurugram"<?php if($city ==='gurugram'){ echo "selected";} ?>>Gurugram</option>
+               <option value="pune"<?php if($city ==='pune'){ echo "selected";} ?>>Pune</option>
+               <option value="bengaluru"<?php if($city ==='bengaluru'){ echo "selected";} ?>>Bengaluru</option>
+               <option value="ahemdabad"<?php if($city ==='ahemdabad'){ echo "selected";} ?>>Ahemdabad</option>
+               <option value="nagpur"<?php if($city ==='nagpur'){ echo "selected";} ?>>Nagpur</option>
+          </select>
+     </div>
+     <div>
+          <label for="state">State</label>
+          <select id="state" name="state">
+               <option value="delhi"<?php if($SN ==='delhi'){ echo "selected";} ?>>Delhi</option>
+               <option value="maharashtra"<?php if($SN ==='maharashtra'){ echo "selected";} ?>>Maharashtra</option>
+               <option value="tamil nadu"<?php if($SN ==='tamil nadu'){ echo "selected";} ?>>Tamil Nadu</option>
+               <option value="west bengal"<?php if($SN ==='west bengal'){ echo "selected";} ?>>west bengal</option>
+               <option value="haryana"<?php if($SN ==='haryana'){ echo "selected";} ?>>Haryana</option>
+               <option value="karnataka"<?php if($SN ==='karnataka'){ echo "selected";} ?>>Karnataka</option>
+               <option value="Gujarat"<?php if($SN ==='Gujarat'){ echo "selected";} ?>>Gujarat</option>
+          </select>
+     </div><br>
+     <div>
+          <legend>Employment Type</legend>
+          <label><input type="radio" value="salaried" name="employment_type"<?php if($ET === "salaried" ){ echo "checked";} ?>>Salaried</label>
+          <label><input type="radio" value="Self employed" name="employment_type"<?php if($ET === "Self employed" ){ echo "checked";} ?>>Self employed</label>
+          <label><input type="radio" value="unemployed" name="employment_type"<?php if($ET === "unemployed" ){ echo "checked";} ?>>Not employed</label>
+
+     </div><br>
+     <div>
+          <legend>Existing Loan</legend>
+          <label><input type="radio" name="existing_loan" value="no"<?php if($loan === "no" ){ echo "checked";} ?>>no</label><br>
+          <label><input type="radio" name="existing_loan" value="yes"<?php if($loan === "yes" ){ echo "checked";} ?>>yes</label><br>
+
+     </div><br>
+
+     <button type="submit">save</ah></button>
+
+
+
+
+     <div class="container">
+          <table class="table">
+               <thead>
+               <tr>
+                    <th scope="col">Id</th>
+                    <th scope="col">Lead name</th>
+                    <th scope="col">Contact number</th>
+                    <th scope="col">Address</th>
+                    <th scope="col">City</th>
+                    <th scope="col">State</th>
+                    <th scope="col">Employment</th>
+                    <th scope="col">Existing loan</th>
+               </tr>
+               </thead>
+               <tbody>
+               <?php
+               require "config.php";
+               $query = "select * from lead_data";
+               $result = mysqli_query($connection, $query);
+               while($row=mysqli_fetch_assoc($result))
+               {
+                    $id=$row['id'];
+                    $name=$row['Lead_name'];
+                    $number=$row['Contact_number'];
+                    $address=$row['Address'];
+                    $city=$row['City'];
+                    $state=$row['State_name'];
+                    $employment_status=$row['Employment_type'];
+                    $existing_loan=$row['Loan_status'];
+                    echo"
+                                            <tr>
+                                                <th scope='row'>$id</th>
+                                                    <td>$name</td>
+                                                    <td>$number</td>
+                                                    <td>$address</td>
+                                                    <td>$city</td>
+                                                    <td>$state</td>
+                                                    <td>$employment_status</td>
+                                                    <td>$existing_loan</td>
+                                                    <td><button><a href='index.php?update_id=$id'>Update</a></button></td>
+                                                    <td><button><a href='delete.php?delete_id=$id'>Delete</a></button></td>
+                                            </tr>
+                                            ";
+               }?>
+
+               </tbody>
+          </table>
+     </div>
 
 
 
 </body>
-
-
-
 </html>
