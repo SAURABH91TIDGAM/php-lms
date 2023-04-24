074f049bccdab10ed8c44d06cfa76d7690fdaa9a
SAURABH91TIDGAM <saurabhtidgam1991@gmail.com>
2023-04-24 15:47:45 +0530

    Update process-form.php
    no need of process update form seprately

--- process-form.php
+++ process-form.php
@@ -1,52 +1,114 @@
 <?php
 require "config.php";
 
+
 function validating($phone){
-    if( preg_match('/^[0-9]{10}+$/', $phone))
+    if(preg_match('/^[0-9]{10}+$/', $phone))
     {
-       return True;
+        return True;
     }
     else{
         return false;
     }
 }
+
+$num=$_GET['id'];
+
+echo $num;
+
 $name = $_POST['name'];
 $contact_number = $_POST['contact_number'];
-$valid = validating($contact_number);
-
-
+$valid_number = validating($contact_number);
 $address = $_POST['address'];
 $city = $_POST['city'];
 $state = $_POST['state'];
 $employment_status = $_POST['employment_type'];
 $Loan_status = $_POST['existing_loan'];
 
+if(isset($_GET['id']))
+{
+    if ($valid_number)
+    {
+        $sql_query = "UPDATE lead_data SET Lead_name='$name', Contact_number='$contact_number', Address='$address', City='$city',
+                     State_name='$state',Employment_type='$employment_status', Loan_status='$Loan_status' where id='$num'";
+
+        $data = mysqli_query($connection, $sql_query);
+
+        if($data)
+        {
+            echo "updated successfully";
+        }
+        else
+        {
+            echo "failed";
+        }
+
+        header('location:index.php');
+        echo "updated successfully";
+    }
+    else
+    {
+        echo "you provided invalid phone number. ";
+        echo "<a href='index.php?update_id=$num'>GO BACK</a>";
+    }
+    if ($valid_number)
+    {
+        $sql_query = "UPDATE lead_data SET Lead_name='$name', Contact_number='$contact_number', Address='$address', City='$city',
+                     State_name='$state',Employment_type='$employment_status', Loan_status='$Loan_status' where id='$num'";
+
+        $data = mysqli_query($connection, $sql_query);
+
+        if($data)
+        {
+            echo "updated successfully";
+        }
+        else
+        {
+            echo "failed";
+        }
+
+        header('location:index.php');
+        //echo "updated successfully";
+    }
+    else
+    {
+        echo "you provided invalid phone number. ";
+        echo "<a href='index.php?update_id=$num'>GO BACK</a>";
+    }
 
 
-if ($valid){
-
-    $sql = "INSERT INTO lead_data(Lead_Name, Contact_number, Address, City, State_name, Employment_type, Loan_status)
+}
+else
+{
+    if($valid_number)
+    {
+        $sql = "INSERT INTO lead_data(Lead_Name, Contact_number, Address, City, State_name, Employment_type, Loan_status)
         VALUES (?,?,?,?,?,?,?)";
 
+//var_dump($name,$contact_number,$address,$city,$state,$employment_status,$Loan_status);
 
-    $stmt =mysqli_stmt_init($connection);
+        $stmt =mysqli_stmt_init($connection);
 
-    if( ! mysqli_stmt_prepare($stmt,$sql)){
-        die(mysqli_error($connection));
-    };
+        if( ! mysqli_stmt_prepare($stmt,$sql)){
+            die(mysqli_error($connection));
+        };
 
-    mysqli_stmt_bind_param($stmt, "sisssss",$name,$contact_number, $address, $city, $state, $employment_status, $Loan_status);
+        mysqli_stmt_bind_param($stmt, "sisssss",$name,$contact_number, $address, $city, $state, $employment_status, $Loan_status);
 
-    mysqli_stmt_execute($stmt);
+        mysqli_stmt_execute($stmt);
 
 //echo "record saved";
-    header('location:index.php');
+        header('location:index.php');
 
+    }
+
+    else{
+        echo "you did not provided valid phone number. ";
+        echo "<a href='index.php'>GO BACK</a>";
+    }
 }
-else{
-    echo "you provided invalid phone number. ";
-    echo "<a href='add_lead_form.php'>GO BACK</a>";
-}
+
+
 
 
 
